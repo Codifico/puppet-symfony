63f34949001b37245e8cd3d1804cbc2e2782093d
Marcin Dryka <marcin@dryka.pl>
2013-10-23 11:05:33 +0200

    Run apt-get update onlyif condition

--- modules/ubuntu/manifests/init.pp
+++ modules/ubuntu/manifests/init.pp
@@ -1,6 +1,7 @@
 class ubuntu {
     exec { 'apt-get-update':
         path => '/usr/bin',
-        command => 'apt-get update'
+        command => 'apt-get update',
+        onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'"
     }
 }
